6b8b33b69cf9cefd12ced559ccc1d61903fccf6b
Bret Pehrson <bretutah@gmail.com>
2025-04-16 21:14:05 -0600

    Few small updates

--- NEPlumbingInc/GlobalUsings.cs
+++ NEPlumbingInc/GlobalUsings.cs
@@ -10,3 +10,5 @@ global using System.Net;
 global using System.Net.Mail;
 global using System.Threading.Tasks;
 global using Microsoft.Extensions.Options;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication;
