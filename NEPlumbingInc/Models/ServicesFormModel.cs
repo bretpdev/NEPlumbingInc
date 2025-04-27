ff268ff3c036017307ea52ff68141a0f7b9cea74
Bret Pehrson <bretutah@gmail.com>
2025-04-27 16:23:04 -0600

    Making a few adjustments and clearing out the AdminDashboard that isn't working.

--- NEPlumbingInc/Models/ServicesFormModel.cs
+++ NEPlumbingInc/Models/ServicesFormModel.cs
@@ -1,8 +1,9 @@
 public class ServicesFormModel
 {
+    [Key]
     public int Id { get; set; }
-    public required string ServiceName { get; set; }
-    public string? ServiceDescription { get; set; }
+    public string? ServiceName { get; set; } = string.Empty;
+    public string? ServiceDescription { get; set; } = string.Empty;
     public string? ServiceImage { get; set; }
     public bool IsActive { get; set; } = true;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
